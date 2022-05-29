0141bce180fc8fe86b025ff15979b68e8131bb40
Sean McLennan <75639634+seanmclennan@users.noreply.github.com>
2022-05-29 09:19:38 -0700

    first commit

---
+++ src/exampleConfig.js
@@ -0,0 +1,5 @@
+{
+    token = ""
+}
+
+module.exports = token
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { SapphireClient } = require('@sapphire/framework')
+const token = require("./config")
+
+const client = new SapphireClient({
+    intents: ['GUILDS', 'GUILD_MESSAGES']
+})
+
+client.login(token)
+console.log("Successfully logged in...")
